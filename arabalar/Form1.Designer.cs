50d91be9773d5e94367815ad4a4731eef31f7ca4
mfu97 <mfu97@hotmail.com>
2021-11-14 19:54:00 +0300

    Initial Commit

---
+++ arabalar/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace arabalar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gecir = new System.Windows.Forms.Button();
+            this.modeller = new System.Windows.Forms.ComboBox();
+            this.sec = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.yil_baslangic = new System.Windows.Forms.TextBox();
+            this.km_bitis = new System.Windows.Forms.TextBox();
+            this.km_baslangic = new System.Windows.Forms.TextBox();
+            this.yil_bitis = new System.Windows.Forms.TextBox();
+            this.sec_2 = new System.Windows.Forms.Button();
+            this.sec_1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(843, 273);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // gecir
+            // 
+            this.gecir.Location = new System.Drawing.Point(901, 12);
+            this.gecir.Name = "gecir";
+            this.gecir.Size = new System.Drawing.Size(75, 23);
+            this.gecir.TabIndex = 1;
+            this.gecir.Text = "Marka";
+            this.gecir.UseVisualStyleBackColor = true;
+            this.gecir.Click += new System.EventHandler(this.gecir_Click);
+            // 
+            // modeller
+            // 
+            this.modeller.FormattingEnabled = true;
+            this.modeller.Location = new System.Drawing.Point(996, 11);
+            this.modeller.Name = "modeller";
+            this.modeller.Size = new System.Drawing.Size(121, 24);
+            this.modeller.TabIndex = 2;
+            this.modeller.SelectedIndexChanged += new System.EventHandler(this.modeller_SelectedIndexChanged);
+            // 
+            // sec
+            // 
+            this.sec.Location = new System.Drawing.Point(1134, 12);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(75, 23);
+            this.sec.TabIndex = 3;
+            this.sec.Text = "Listele";
+            this.sec.UseVisualStyleBackColor = true;
+            this.sec.Click += new System.EventHandler(this.sec_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(12, 322);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(843, 273);
+            this.dataGridView3.TabIndex = 5;
+            // 
+            // yil_baslangic
+            // 
+            this.yil_baslangic.Location = new System.Drawing.Point(901, 359);
+            this.yil_baslangic.Name = "yil_baslangic";
+            this.yil_baslangic.Size = new System.Drawing.Size(100, 22);
+            this.yil_baslangic.TabIndex = 6;
+            // 
+            // km_bitis
+            // 
+            this.km_bitis.Location = new System.Drawing.Point(1134, 408);
+            this.km_bitis.Name = "km_bitis";
+            this.km_bitis.Size = new System.Drawing.Size(100, 22);
+            this.km_bitis.TabIndex = 7;
+            // 
+            // km_baslangic
+            // 
+            this.km_baslangic.Location = new System.Drawing.Point(1134, 359);
+            this.km_baslangic.Name = "km_baslangic";
+            this.km_baslangic.Size = new System.Drawing.Size(100, 22);
+            this.km_baslangic.TabIndex = 8;
+            // 
+            // yil_bitis
+            // 
+            this.yil_bitis.Location = new System.Drawing.Point(901, 409);
+            this.yil_bitis.Name = "yil_bitis";
+            this.yil_bitis.Size = new System.Drawing.Size(100, 22);
+            this.yil_bitis.TabIndex = 9;
+            // 
+            // sec_2
+            // 
+            this.sec_2.Location = new System.Drawing.Point(1134, 454);
+            this.sec_2.Name = "sec_2";
+            this.sec_2.Size = new System.Drawing.Size(75, 23);
+            this.sec_2.TabIndex = 10;
+            this.sec_2.Text = "Seç";
+            this.sec_2.UseVisualStyleBackColor = true;
+            this.sec_2.Click += new System.EventHandler(this.sec_2_Click);
+            // 
+            // sec_1
+            // 
+            this.sec_1.Location = new System.Drawing.Point(901, 454);
+            this.sec_1.Name = "sec_1";
+            this.sec_1.Size = new System.Drawing.Size(75, 23);
+            this.sec_1.TabIndex = 11;
+            this.sec_1.Text = "Seç";
+            this.sec_1.UseVisualStyleBackColor = true;
+            this.sec_1.Click += new System.EventHandler(this.sec_1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(898, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Yıl aralığı seçiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1131, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Km aralığı seçiniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 607);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sec_1);
+            this.Controls.Add(this.sec_2);
+            this.Controls.Add(this.yil_bitis);
+            this.Controls.Add(this.km_baslangic);
+            this.Controls.Add(this.km_bitis);
+            this.Controls.Add(this.yil_baslangic);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.sec);
+            this.Controls.Add(this.modeller);
+            this.Controls.Add(this.gecir);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button gecir;
+        private System.Windows.Forms.ComboBox modeller;
+        private System.Windows.Forms.Button sec;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TextBox yil_baslangic;
+        private System.Windows.Forms.TextBox km_bitis;
+        private System.Windows.Forms.TextBox km_baslangic;
+        private System.Windows.Forms.TextBox yil_bitis;
+        private System.Windows.Forms.Button sec_2;
+        private System.Windows.Forms.Button sec_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ arabalar/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arabalar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable tablo = new DataTable();
+        DataTable yenitablo = new DataTable();
+        public int aranan_r = 0;
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = tablo;
+            tablo.Columns.Add("Adı", typeof(string));
+            tablo.Columns.Add("Modeli", typeof(string));
+            tablo.Columns.Add("Km", typeof(string));
+            tablo.Columns.Add("Yılı", typeof(string));
+
+            tablo.Rows.Add("Mercedes", "cla 200", "150000", "2016");
+            tablo.Rows.Add("Mercedes", "e 200", "300000", "2013");
+            tablo.Rows.Add("Mercedes", "gla 200", "110000", "2015");
+            tablo.Rows.Add("Mercedes", "e 250", "250000", "2010");
+            tablo.Rows.Add("Mercedes", "b 180", "190000", "2014");
+            tablo.Rows.Add("Toyota", "Corolla", "200000", "2009");
+            tablo.Rows.Add("Toyota", "Auris", "100000", "2015");
+
+            yenitablo.Columns.Add("Adı", typeof(string));
+            yenitablo.Columns.Add("Modeli", typeof(string));
+            yenitablo.Columns.Add("Km", typeof(string));
+            yenitablo.Columns.Add("Yılı", typeof(string));
+
+        }
+
+        private void modeller_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gecir_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i <= dataGridView1.Rows.Count - 2; i++)
+            {
+                modeller.Items.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+
+            }
+            string[] items = modeller.Items.OfType<string>().Distinct().ToArray();
+            modeller.Items.Clear();
+            for (int i = 0; i < items.Length; i++)
+                modeller.Items.Add(items[i]);
+        }
+
+        private void sec_Click(object sender, EventArgs e)
+        {
+            dataGridView3.DataSource = yenitablo;
+            string aranan = modeller.Text.Trim().ToUpper();
+
+
+            int i = 0;
+            aranan_r = 0;
+            string ad, model, km, yil;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in dataGridView1.Rows[i].Cells)
+                {
+                    if (cell.Value != null)
+                    {
+                        if (cell.Value.ToString().ToUpper() == aranan)
+                        {
+                            ad = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                            model = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                            km = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                            yil = dataGridView1.Rows[i].Cells[3].Value.ToString();
+                            yenitablo.Rows.Add(ad, model, km, yil);
+                        }
+                    }
+                }
+                i = i + 1;
+            }
+            modeller.Text = ("");
+            modeller.Items.Clear();
+        }
+
+        private void sec_1_Click(object sender, EventArgs e)
+        {
+            //int aranan = yil_baslangic.Text.Trim().ToUpper();
+            int sayac = dataGridView3.Rows.Count;
+            int deger = 0;
+            int i = 0;
+            int yil1, yil2;
+            for (i = 0; i <= sayac - 2; i++)
+            {
+                try
+                {
+                    deger = Convert.ToInt32(dataGridView3.Rows[i].Cells[3].Value.ToString());
+                    yil1 = Convert.ToInt32(this.yil_baslangic.Text);
+                    yil2 = Convert.ToInt32(this.yil_bitis.Text);
+                    if (deger >= yil1 && deger <= yil2)
+                    {
+
+                    }
+                    else
+                    {
+                        dataGridView3.Rows.RemoveAt(this.dataGridView3.Rows[i].Index);
+                        i--;
+                    }
+                }
+                catch
+                {
+
+                }
+
+            }
+
+        }
+
+        private void sec_2_Click(object sender, EventArgs e)
+        {
+            int sayac = dataGridView3.Rows.Count;
+            int deger = 0;
+            int i = 0;
+            int Km1, Km2;
+            for (i = 0; i <= sayac - 2; i++)
+            {
+                try
+                {
+                    deger = Convert.ToInt32(dataGridView3.Rows[i].Cells[2].Value.ToString());
+                    Km1 = Convert.ToInt32(this.km_baslangic.Text);
+                    Km2 = Convert.ToInt32(this.km_bitis.Text);
+                    if (deger >= Km1 && deger <= Km2)
+                    {
+
+                    }
+                    else
+                    {
+                        dataGridView3.Rows.RemoveAt(this.dataGridView3.Rows[i].Index);
+                        i--;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
